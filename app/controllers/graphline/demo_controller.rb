5d6ef410ef31f5e22f9567b74d658978ae764d99
Andrew Fallows <kaldrenon@gmail.com>
2014-08-25 13:32:37 -0400

    table shows colors dynamically

--- app/controllers/graphline/demo_controller.rb
+++ app/controllers/graphline/demo_controller.rb
@@ -3,35 +3,34 @@ require_dependency "graphline/application_controller"
 module Graphline
   class DemoController < ApplicationController
     def index
-      # TODO: set up demo segment values
       @total = 265;
       @segments = [
         {
           name: 'Data Bucket One',
           database_id: 1,
           count: 20,
-          html_id:  'bucket_one',
+          html_id:  'bucket_1',
           color: '#ABC'
         },
         {
           name: 'Data Bucket Two',
           database_id: 2,
           count: 25,
-          html_id:  'bucket_two',
+          html_id:  'bucket_2',
           color: '#CBA'
         },
         {
           name: 'Data Bucket Three',
           database_id: 3,
           count: 100,
-          html_id:  'bucket_three',
+          html_id:  'bucket_3',
           color: '#ACB'
         },
         {
           name: 'Data Bucket Four',
           database_id: 4,
           count: 75,
-          html_id:  'bucket_four',
+          html_id:  'bucket_4',
           color: '#BCA'
         },
         {
